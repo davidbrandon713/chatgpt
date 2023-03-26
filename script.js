e4cfef9c66f2b6a034e1319eabc25111ca8776a1
David <davidbrandon713@gmail.com>
2023-03-26 04:57:28 -0500

    fix spaces issue

--- script.js
+++ script.js
@@ -7,62 +7,62 @@ config()
 process.title = 'ChatGPT'
 const model = 'gpt-3.5-turbo'
 const colors = {
-	red: `\x1b[31m`,
-	green: `\x1b[32m`,
-	yellow: `\x1b[33m`,
-	blue: `\x1b[34m`,
-	magenta: `\x1b[35m`,
-	cyan: `\x1b[36m`,
-	white: `\x1b[37m`,
-	default: `\x1b[39m`,
+  red: `\x1b[31m`,
+  green: `\x1b[32m`,
+  yellow: `\x1b[33m`,
+  blue: `\x1b[34m`,
+  magenta: `\x1b[35m`,
+  cyan: `\x1b[36m`,
+  white: `\x1b[37m`,
+  default: `\x1b[39m`,
 
-	lightred: `\x1b[91m`,
-	lightgreen: `\x1b[92m`,
-	lightyellow: `\x1b[93m`,
-	lightblue: `\x1b[94m`,
-	lightmagenta: `\x1b[95m`,
-	lightcyan: `\x1b[96m`,
+  lightred: `\x1b[91m`,
+  lightgreen: `\x1b[92m`,
+  lightyellow: `\x1b[93m`,
+  lightblue: `\x1b[94m`,
+  lightmagenta: `\x1b[95m`,
+  lightcyan: `\x1b[96m`,
 }
 const color = colors.lightcyan
 const helpText = `\nhelp --- show commands\nclear -- clear log\n`
 
 const openai = new OpenAIApi(
-	new Configuration({
-		apiKey: process.env.API_KEY,
-	})
+  new Configuration({
+    apiKey: process.env.API_KEY,
+  })
 )
 
 const userInterface = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 })
 
 // Custom message
 const beginConversation = () => {
-	console.log('Starting a conversation with ChatGPT using gpt-3.5-turbo')
-	console.log('Type "help" to view commands\n\n')
-	userInterface.prompt()
+  console.log('Starting a conversation with ChatGPT using gpt-3.5-turbo')
+  console.log('Type "help" to view commands\n\n')
+  userInterface.prompt()
 }
 
 beginConversation()
 userInterface.on('line', async (input) => {
-	// Put special case inputs here like 'help' or 'version'
-	// Print help
-	if (input == 'help') {
-		console.log(helpText)
-		return userInterface.prompt()
-	}
+  // Put special case inputs here like 'help' or 'version'
+  // Print help
+  if (input == 'help') {
+    console.log(helpText)
+    return userInterface.prompt()
+  }
 
-	// Clear the shell
-	if (input == 'clear') {
-		console.clear()
-		return beginConversation()
-	}
+  // Clear the shell
+  if (input == 'clear') {
+    console.clear()
+    return beginConversation()
+  }
 
-	const res = await openai.createChatCompletion({
-		model: model,
-		messages: [{ role: 'user', content: input }],
-	})
-	console.log(`\n${color}>>`, res.data.choices[0].message.content, `\x1b[0m\n`)
-	userInterface.prompt()
+  const res = await openai.createChatCompletion({
+    model: model,
+    messages: [{ role: 'user', content: input }],
+  })
+  console.log(`\n${color}>>`, res.data.choices[0].message.content, `\x1b[0m\n`)
+  userInterface.prompt()
 })
